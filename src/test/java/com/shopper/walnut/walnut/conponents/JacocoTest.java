daef48c57828a876d8b00b77e95433b00c773973
chandle <phc09188@gmail.com>
2022-10-23 21:18:10 +0900

    jacocoTestReport.xml생성

---
+++ src/test/java/com/shopper/walnut/walnut/conponents/JacocoTest.java
@@ -0,0 +1,14 @@
+package com.shopper.walnut.walnut.conponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class JacocoTest {
+    @Test
+    void test (){
+        assertEquals(1,1);
+    }
+}

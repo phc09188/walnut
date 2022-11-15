64c6e55c7f9d95fbc0233e2b9f762e0eab74541d
chandle <phc09188@gmail.com>
2022-11-15 10:49:14 +0900

    page util 생성

--- src/main/java/com/shopper/walnut/walnut/controller/MainController.java
+++ src/main/java/com/shopper/walnut/walnut/controller/MainController.java
@@ -1,29 +1,18 @@
 package com.shopper.walnut.walnut.controller;
 
-
-import com.shopper.walnut.walnut.model.entity.BrandItem;
-import com.shopper.walnut.walnut.model.entity.Category;
+import com.shopper.walnut.walnut.model.constant.CacheKey;
 import com.shopper.walnut.walnut.model.entity.Item;
-import com.shopper.walnut.walnut.model.input.CategoryName;
-import com.shopper.walnut.walnut.repository.BrandItemRepository;
-import com.shopper.walnut.walnut.repository.CategoryRepository;
-import com.shopper.walnut.walnut.repository.ItemRepository;
 import com.shopper.walnut.walnut.service.CategoryService;
 import com.shopper.walnut.walnut.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -32,25 +21,21 @@ public class MainController {
     private final ItemService itemService;
 
     //메인
+    @Cacheable(key = "", value = CacheKey.MAIN_PAGE_SCHEDULE)
     @RequestMapping("/")
-    public String index(Model model, @PageableDefault(page=0, size=10, sort="reviewScore",
-    direction = Sort.Direction.DESC)Pageable pageable){
+    public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "reviewScore",
+            direction = Sort.Direction.DESC) Pageable pageable) {
         Page<Item> items = itemService.getAllList(pageable);
-        int nowPage = items.getPageable().getPageNumber()+1 ;
+        int nowPage = items.getPageable().getPageNumber() + 1;
         int totalNum = items.getTotalPages();
         int startPage = totalNum < 5 ? 1 : totalNum - 4;
         model.addAttribute("nowPage", nowPage);
         model.addAttribute("totalNum", totalNum);
         model.addAttribute("startPage", startPage);
         model.addAttribute("items", items);
-        model.addAttribute("categoryNames",categoryService.getCategoryNames());
+        model.addAttribute("categoryNames", categoryService.getCategoryNames());
         return "/index";
     }
-
-
-
-
-
 
 
 }
---
+++ src/main/java/com/shopper/walnut/walnut/model/constant/Pagination.java
@@ -0,0 +1,91 @@
+package com.shopper.walnut.walnut.model.constant;
+
+
+import lombok.Data;
+
+@Data
+public class Pagination {
+    /**
+     *  페이지 당 보여지는 게시글 최대 갯수
+     */
+    private int pageSize = 10;
+    /**
+     *  페이징된 버튼의 블럭당 최대 개수
+     */
+    private int blockSize = 5;
+    /**
+     *  현재 페이지
+     */
+    private int page = 1;
+    /**
+     *  현재 블럭
+     */
+    private int block = 1;
+    /**
+     *  총 게시글 수
+     */
+    private int totalListCnt;
+    /**
+     *  총 페이지 수
+     */
+    private int totalPageCnt;
+    /**
+     *  총 블럭 수
+     */
+    private int totalBlockCnt;
+    /**
+     *  블럭 시작 페이지
+     */
+    private int startPage = 1;
+    /**
+     *  블럭 마지막 페이지
+     */
+    private int endPage = 1;
+    /**
+     *  이전 블럭 마지막 페이지
+     */
+    private int prevBlock;
+    /**
+     *  다음 블럭 시작 페이지
+     */
+    private int nextBlock;
+
+    /**
+     *  DB 접근 시작 index
+     */
+    private int startIndex = 0;
+
+    /**
+     *  이전 페이지 여부
+     */
+    private boolean prev;
+    /**
+     *  다음 페이지 여부
+     */
+    private boolean next;
+
+    public Pagination(int totalListCnt, int page) {
+        setPage(page);
+        setTotalListCnt(totalListCnt);
+        setTotalPageCnt((int) Math.ceil(totalListCnt * 1.0 / pageSize));
+        setTotalBlockCnt((int) Math.ceil(totalPageCnt * 1.0 / blockSize));
+        setBlock((int) Math.ceil((page * 1.0)/blockSize));
+        setStartPage((block - 1) * blockSize + 1);
+        setEndPage(startPage + blockSize - 1);
+
+        if(endPage > totalPageCnt){
+            this.endPage = totalPageCnt;
+        }
+        setPrevBlock((block * blockSize) - blockSize);
+
+        if(prevBlock < 1) {this.prevBlock = 1;}
+
+        setNextBlock((block * blockSize) + 1);
+
+        /* === 다음 블럭에 대한 validation ===*/
+        if(nextBlock > totalPageCnt) {nextBlock = totalPageCnt;}
+        this.next = endPage == totalPageCnt ? false : true;
+        this.prev = startPage == 1 ? false : true;
+        setStartIndex((page-1) * pageSize);
+    }
+}
